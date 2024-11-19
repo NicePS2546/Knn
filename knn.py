62a40b1daf0617643fba20c12d989b1d64898b2e
maoezx056 <maoezxooo@gmail.com>
2024-11-19 15:42:03 +0700

    add file

--- knn.py
+++ knn.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+import numpy as np
 
 st.header("การจำแนกข้อมูลดอกไม้")
 st.image('./img/NiceMewing.jpg')
