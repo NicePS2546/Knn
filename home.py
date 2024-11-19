206b48defd222c0ef6d00348e25c3b6a6cde8748
maoezx056 <maoezxooo@gmail.com>
2024-11-19 15:15:41 +0700

    add file

--- home.py
+++ home.py
@@ -52,7 +52,8 @@ labels = ['Men', 'Women','','']
 sizes = [35,25,15,25]
 explode = (0, 0.1,0,0) 
 fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',shadow=True, startangle=90)
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
 st.pyplot(fig1)
 st.write('ค่าน้อยที่สุด')
 cl31,cl32,cl33,cl34=st.columns(4)
