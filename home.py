4af50332d21437d9efd3c785125bc586363a4bf7
maoezx056 <maoezxooo@gmail.com>
2024-11-19 14:46:36 +0700

    add file

--- home.py
+++ home.py
@@ -4,4 +4,9 @@ import pandas as pd
 st.title("🥨🥨WedSite Developing using Python🥨🥨")
 st.header("🥪WedSite Developing using Python🥪")
 
+st.image('./img/Nice.jpg')
 st.subheader("Nice Lnwza007")
+
+dt = pd.read_csv('./data/iris.csv')
+st.header()
+st.write(dt.head(10))
